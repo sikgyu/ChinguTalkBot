e311f463635223af5b6718ea2e074bd5e3581d4c
Sikgyu <dlatlrrb@github.com>
2024-03-08 18:03:34 -0800

    Add CDK Cognito

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Auth } from "./constructs/auth";
+
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class ChinguTalkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+    const auth = new Auth(this, "Auth");
+  }
+}
