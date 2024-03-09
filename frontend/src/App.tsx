a805b9d954da3d3ede43193e5f934956261a49b6
Sikgyu <dlatlrrb@github.com>
2024-03-09 09:29:27 -0800

    Add frontend - create Authenticator using amplify

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({
+  Auth: {
+    userPoolId: import.meta.env.VITE_APP_USER_POOL_ID,
+    userPoolWebClientId: import.meta.env.VITE_APP_USER_POOL_CLIENT_ID,
+    authenticationFlowType: 'USER_SRP_AUTH',
+  },
+});
+
+
+function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <header className="App-header">
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </header>
+        </div>
+      )}
+    </Authenticator>
+  );
+}
+
+export default App;
