9ca1ea067da13a0a82a7210e158b85e09c3b3e9e
Sikgyu <dlatlrrb@github.com>
2024-03-14 15:05:46 -0700

    Create Lambda

--- cdk/lib/constructs/api.ts
+++ cdk/lib/constructs/api.ts
@@ -2,9 +2,13 @@ import { Construct } from "constructs";
 import { ITable } from "aws-cdk-lib/aws-dynamodb";
 import { HttpApi, CorsHttpMethod, HttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
 import { HttpUserPoolAuthorizer } from 'aws-cdk-lib/aws-apigatewayv2-authorizers';
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import { DockerImageCode, DockerImageFunction } from "aws-cdk-lib/aws-lambda";
 import { CfnOutput, Duration } from "aws-cdk-lib";
 import { Auth } from "./auth";
-
+import { Platform } from "aws-cdk-lib/aws-ecr-assets";
+import { Stack } from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export interface ApiProps {
     readonly database: ITable;
@@ -15,15 +19,46 @@ export interface ApiProps {
   }
 
 
-export class Api extends Construct {
+  export class Api extends Construct {
     readonly api: HttpApi;
     constructor(scope: Construct, id: string, props: ApiProps) {
       super(scope, id);
   
       const { database, corsAllowOrigins: allowOrigins = ["*"] } = props;
-
   
-      const api = new HttpApi(this, "HttpApi", {
+      const handler = new DockerImageFunction(this, "Handler", {
+        code: DockerImageCode.fromImageAsset("../backend", {
+          platform: Platform.LINUX_AMD64,
+        }),
+        memorySize: 256,
+        timeout: Duration.seconds(30),
+        environment: {
+          TABLE_NAME: database.tableName,
+          CORS_ALLOW_ORIGINS: allowOrigins.join(","),
+          USER_POOL_ID: props.auth.userPool.userPoolId,
+          CLIENT_ID: props.auth.client.userPoolClientId,
+          REGION: Stack.of(this).region,
+          BEDROCK_REGION: props.bedrockRegion,
+          ENDPOINT_URL: props.bedrockEndpointUrl,
+        },
+      });
+      handler.role?.attachInlinePolicy(
+        new iam.Policy(this, "BedrockPolicy", {
+          statements: [
+            new iam.PolicyStatement({
+              actions: ["bedrock:*"],
+              resources: ["*"],
+            }),
+          ],
+        })
+      );
+      handler.role?.grantAssumeRole(
+        new iam.ServicePrincipal("bedrock.amazonaws.com")
+      );
+  
+      database.grantReadWriteData(handler);
+  
+      const api = new HttpApi(this, "Default", {
         corsPreflight: {
           allowHeaders: ["*"],
           allowMethods: [
@@ -40,6 +75,7 @@ export class Api extends Construct {
         },
       });
   
+      const integration = new HttpLambdaIntegration("Integration", handler);
       const authorizer = new HttpUserPoolAuthorizer(
         "Authorizer",
         props.auth.userPool,
@@ -49,6 +85,7 @@ export class Api extends Construct {
       );
       let routeProps: any = {
         path: "/{proxy+}",
+        integration,
         methods: [
           HttpMethod.GET,
           HttpMethod.POST,
@@ -58,7 +95,9 @@ export class Api extends Construct {
         ],
         authorizer,
       };
-    
+  
+      api.addRoutes(routeProps);
+  
       this.api = api;
   
       new CfnOutput(this, "BackendApiUrl", { value: api.apiEndpoint });
