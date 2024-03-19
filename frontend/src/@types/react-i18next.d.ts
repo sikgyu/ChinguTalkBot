6193a2704d9992f8406180c6f0346dab3d368174
Sikgyu <dlatlrrb@github.com>
2024-03-19 00:06:28 -0700

    .

--- frontend/src/@types/react-i18next.d.ts
+++
@@ -1,7 +0,0 @@
-import "react-i18next";
-import en from "../i18n/en";
-declare module "i18next" {
-  interface CustomTypeOptions {
-    resources: typeof en;
-  }
-}
--- frontend/src/utils/BotUtils.ts
+++
@@ -1,9 +0,0 @@
-import copy from 'copy-to-clipboard';
-
-export const getBotUrl = (botId: string) => {
-  return `${window.location.origin}/bot/${botId}`;
-};
-
-export const copyBotUrl = (botId: string) => {
-  copy(getBotUrl(botId));
-};
